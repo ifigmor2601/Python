5207e3b08b7c261cda6a804d66a390f4c4bcc093
ifigmor2601 <118441838+ifigmor2601@users.noreply.github.com>
2022-11-17 12:05:55 +0100

    Add files via upload

---
+++ PruebasConexion.py
@@ -0,0 +1,9 @@
+import Conexion
+from datetime import datetime
+
+Conexion.alter()
+
+Conexion.crearTabla()
+
+Conexion.insertar('0001-R','Raton',30.5,500,datetime(2022,12,15,21,30),'Raton gaming inalambrico')
+
